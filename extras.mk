219f44917949b9b306a679ba7efa6abb1ebd7c69
Luofan Chen <clfbbn@gmail.com>
2026-04-06 05:17:12 +0000

    extras: Import framework compatibility matrix
    Change-Id: I55f3fdec5296d2974f4ac5f93f0cf53493fd5e9c

--- extras.mk
+++ extras.mk
@@ -3,5 +3,8 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# HIDL
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/sony/extras/framework_compatibility_matrix.xml
+
 # Inherit from the proprietary version
 $(call inherit-product, vendor/sony/extras/extras-vendor.mk)
