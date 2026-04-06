41a7ea21a58f47d61690701e711b0f8c5c3d0404
Luofan Chen <clfbbn@gmail.com>
2026-04-06 05:32:49 +0000

    extras: Import Dolby properties
    Change-Id: I7d1cb90e0182eb3ea8c69b7e61c80ece744c9376

--- extras.mk
+++ extras.mk
@@ -3,8 +3,19 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Dolby
+PRODUCT_VENDOR_PROPERTIES += \
+    ro.vendor.dolby.dax.version=DAX3_3.7.0.8_r1 \
+    vendor.audio.dolby.ds2.hardbypass=false \
+    vendor.audio.dolby.ds2.enabled=false
+
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/sony/extras/framework_compatibility_matrix.xml
+
+# Spatial Audio
+PRODUCT_VENDOR_PROPERTIES += \
+    vendor.360ra.effect=1 \
+    ro.audio.spatializer_enabled=true
 
 # SEPolicy
 BOARD_VENDOR_SEPOLICY_DIRS += vendor/sony/extras/sepolicy/vendor
