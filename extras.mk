8b2c0b31ba29467cf3a52ae70055d893b787b5e8
realahnet <syedmusicrealise@gmail.com>
2026-04-06 05:27:45 +0000

    extras: Import SEPolicy rules for 360 RA and Dolby
    Co-authored-by: Luofan Chen <clfbbn@gmail.com>
    Change-Id: I2a2f82866f60d791d8db3dd770813944deefaec0

--- extras.mk
+++ extras.mk
@@ -6,5 +6,8 @@
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/sony/extras/framework_compatibility_matrix.xml
 
+# SEPolicy
+BOARD_VENDOR_SEPOLICY_DIRS += vendor/sony/extras/sepolicy/vendor
+
 # Inherit from the proprietary version
 $(call inherit-product, vendor/sony/extras/extras-vendor.mk)
